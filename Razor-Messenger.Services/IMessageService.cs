29376869b8000bbd3bc4d8d4210134aed4fc89cb
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-25 23:12:41 +0300

    implement message service abstraction

---
+++ Razor-Messenger.Services/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using Razor_Messenger.Data.Models;
+
+namespace Razor_Messenger.Services;
+
+public interface IMessageService
+{
+    Task SendMessageAsync(string sender, string receiver, string message);
+    IEnumerable<Message> GetMessages(string sender, string receiver, int skip, int take);
+}
