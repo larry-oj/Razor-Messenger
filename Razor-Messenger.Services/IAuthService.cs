713db59b784a15c02f70e39d074b6070bf8e29f7
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-25 20:07:49 +0300

    create auth service abstraction

---
+++ Razor-Messenger.Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Razor_Messenger.Data.Models;
+
+namespace Razor_Messenger.Services;
+
+public interface IAuthService
+{
+    Task<User> RegisterAsync(string username, string password);
+    Task<User> LoginAsync(string username, string password);
+    Task<string> HashPasswordAsync(string password);
+}
