a62c2395f630de0c20c120bb0dd1472d192256c6
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-26 11:09:06 +0300

    create MessageService.cs

---
+++ Razor-Messenger.Services/MessageService.cs
@@ -0,0 +1,21 @@
+﻿using Razor_Messenger.Data.Models;
+
+namespace Razor_Messenger.Services;
+
+public class MessageService : IMessageService
+{
+    public async Task SendMessageAsync(string sender, string receiver, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Message> GetLastMessages(string participantOne, string participantTwo, int take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Message> GetLastMessages(string participantOne, string participantTwo, int skip, int take)
+    {
+        throw new NotImplementedException();
+    }
+}
