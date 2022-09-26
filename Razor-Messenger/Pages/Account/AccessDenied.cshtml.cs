15ec17f9c8ea1221fbc3a8d510aea3b53880a060
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-26 14:15:15 +0300

    create access denied page

---
+++ Razor-Messenger/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Messenger.Pages.Account;
+
+public class AccessDenied : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
