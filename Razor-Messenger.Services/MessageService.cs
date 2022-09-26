e601a712f1cd21269cca1f46ebe5ca576d5d4f66
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-26 11:40:29 +0300

    implement message service

--- Razor-Messenger.Services/MessageService.cs
+++ Razor-Messenger.Services/MessageService.cs
@@ -1,21 +1,59 @@
-﻿using Razor_Messenger.Data.Models;
+﻿using Razor_Messenger.Data;
+using Razor_Messenger.Data.Models;
+using Razor_Messenger.Services.Exceptions;
 
 namespace Razor_Messenger.Services;
 
 public class MessageService : IMessageService
 {
+    private readonly MessengerContext _context;
+    
+    public MessageService(
+        MessengerContext context)
+    {
+        _context = context;
+    }
+
     public async Task SendMessageAsync(string sender, string receiver, string message)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(message))
+            throw new EmptyMessageException();
+        if (sender == receiver)
+            throw new MessageToSelfException();
+        
+        var senderUser = _context.Users.FirstOrDefault(u => u.Username == sender)
+                         ?? throw new InvalidSenderException();
+        var receiverUser = _context.Users.FirstOrDefault(u => u.Username == receiver)
+                           ?? throw new InvalidReceiverException();
+        
+        var messageEntity = new Message(senderUser, receiverUser, message);
+        _context.Messages.Add(messageEntity);
+        await _context.SaveChangesAsync();
     }
 
     public IEnumerable<Message> GetLastMessages(string participantOne, string participantTwo, int take)
     {
-        throw new NotImplementedException();
+        return GetLastMessages(participantOne, participantTwo, 0, take);
     }
 
     public IEnumerable<Message> GetLastMessages(string participantOne, string participantTwo, int skip, int take)
     {
-        throw new NotImplementedException();
+        if (participantOne == participantTwo)
+            throw new MessageToSelfException();
+        
+        var userOne = _context.Users.FirstOrDefault(u => u.Username == participantOne) 
+                      ?? throw new InvalidSenderException();
+        var userTwo = _context.Users.FirstOrDefault(u => u.Username == participantTwo) 
+                      ?? throw new InvalidReceiverException();
+
+        var messages = _context.Messages
+            .Where(m => (m.Sender == userOne && m.Receiver == userTwo) || 
+                        (m.Receiver == userOne && m.Sender == userTwo))
+            .OrderByDescending(m => m.SentAt) 
+            .Skip(skip)
+            .Take(take)     // IMPORTANT: TakeLast() is not supported in EF Core
+            .OrderBy(m => m.SentAt);
+
+        return messages;
     }
 }
