dfc3e53618beca57cbb9343829d6b8de979efe7b
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-27 15:28:41 +0300

    implement chatroom with user list

---
+++ Razor-Messenger/Pages/Messenger.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_Messenger.Pages.Shared;
+using Razor_Messenger.Services;
+
+namespace Razor_Messenger.Pages;
+
+[Authorize]
+public class Messenger : PageModel
+{
+    public List<_UserPartial> Users { get; set; }
+    
+    private readonly IUserService _userService;
+    private readonly IMessageService _messageService;
+
+    public Messenger(IUserService userService, 
+        IMessageService messageService)
+    {
+        _userService = userService;
+        _messageService = messageService;
+    }
+    
+    public void OnGet()
+    {
+        Users = new List<_UserPartial>();
+        
+        var currentUserName = base.User.Identity!.Name!;
+        var users = _userService.GetAllUsers(currentUserName).ToList();
+        foreach (var user in users)
+        {
+            var lastMessage = _messageService.GetLastMessages(currentUserName, user.Username, 1).ToList();
+            Users.Add(new _UserPartial
+            {
+                Username = user.Username,
+                LastMessageContent = lastMessage.Count > 0 ? lastMessage[0].Content : "",
+                LastMessageTime = lastMessage.Count > 0 ? lastMessage[0].SentAt : new DateTime(2000, 1, 1, 0, 0, 0)
+            });
+        }
+    }
+}
---
+++ Razor-Messenger/Pages/Shared/_UserPartial.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Messenger.Pages.Shared;
+
+public class _UserPartial
+{
+    public string Username { get; set; }
+    public DateTime LastMessageTime { get; set; }
+    public string LastMessageContent { get; set; }
+
+    public _UserPartial()
+    {
+    }
+    
+    public _UserPartial(string username, DateTime lastMessageTime, string lastMessageContent)
+    {
+        Username = username;
+        LastMessageTime = lastMessageTime;
+        LastMessageContent = lastMessageContent;
+    }
+}
